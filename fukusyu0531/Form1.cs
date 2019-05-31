90ea0b6f0e62faa05124d2bbdf35f2df07997e66
retnton <38481077+retnton@users.noreply.github.com>
2019-05-31 13:31:17 +0900

    復習(未完成

--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -12,9 +12,28 @@ namespace fukusyu0531
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+                label1.Left = vx;
+                label1.Top = vy;
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(label1.Left >= 0)
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
